de58fd2d45797a3514a1dfdafb6e3c9aac3828ec
siotta <alessialongo2005@gmail.com>
2025-02-17 11:45:59 +0100

    terzo_es

---
+++ esercizio-3/idex.php
@@ -0,0 +1,13 @@
+<?php
+$numero1 = 10;
+$numero2 = 5;
+
+$somma = $numero1 + $numero2;
+echo "La somma è: $somma\n";
+
+$numero3 = 4;
+$numero4 = 6;
+
+$multiplicazione = $numero3 * $numero4;
+echo "Il risultato della moltiplicazione è: $multiplicazione";
+?>
